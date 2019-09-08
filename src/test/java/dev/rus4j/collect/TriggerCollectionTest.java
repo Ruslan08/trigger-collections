562ce9ca7c0cc012e5e2084c77219c5f4bd011d5
Ruslan08 <eto_ya08@mail.ru>
2019-09-08 20:58:52 +0300

    add addAll, remove, removeAll operations to trigger

--- src/test/java/dev/rus4j/collect/TriggerCollectionTest.java
+++ src/test/java/dev/rus4j/collect/TriggerCollectionTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -13,6 +14,8 @@ import static org.junit.Assert.assertTrue;
 
 public class TriggerCollectionTest {
     private static final int VALUE_TO_ADD = 7;
+    private static final List<Integer> LIST_TO_ADD = List.of(1, 2, 3);
+    private static final List<Integer> LIST_TO_REMOVE = List.of(2, 3);
     private Collection<Integer> collection;
 
     @Before
@@ -26,23 +29,93 @@ public class TriggerCollectionTest {
             assertEquals(Integer.valueOf(VALUE_TO_ADD), i);
             assertTrue(bool);
         };
+        Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .afterAdd(testConsumer)
+            .build();
 
-        Collection<Integer> triggered = TriggerCollection.from(this.collection)
-                .afterAdd(testConsumer)
-                .build();
-
-        @SuppressWarnings("unused")
-        boolean added = triggered.add(VALUE_TO_ADD);
+        collection.add(VALUE_TO_ADD);
     }
 
     @Test
     public void testBeforeAddTrigger() {
         Consumer<Integer> testConsumer = i -> assertEquals(Integer.valueOf(VALUE_TO_ADD), i);
-        Collection<Integer> triggered = TriggerCollection.from(this.collection)
-                .beforeAdd(testConsumer)
-                .build();
+        Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .beforeAdd(testConsumer)
+            .build();
 
-        @SuppressWarnings("unused")
-        boolean added = triggered.add(VALUE_TO_ADD);
+        collection.add(VALUE_TO_ADD);
+    }
+
+    @Test
+    public void testBeforeAddAllTrigger() {
+        Consumer<Collection<Integer>> testConsumer = collectionToAdd -> assertEquals(LIST_TO_ADD.size(), collectionToAdd.size());
+        Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .beforeAddAll(testConsumer)
+            .build();
+
+        collection.addAll(LIST_TO_ADD);
+    }
+
+    @Test
+    public void testAfterAddAllTrigger() {
+        BiConsumer<Collection<Integer>, Boolean> testConsumer = (collectionToAdd, result) -> {
+            assertEquals(LIST_TO_ADD.size(), collectionToAdd.size());
+            assertTrue(result);
+        };
+        Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .afterAddAll(testConsumer)
+            .build();
+
+        collection.addAll(LIST_TO_ADD);
+    }
+
+    @Test
+    public void testBeforeRemoveTrigger() {
+        Consumer<Integer> testConsumer = valueToRemove -> assertEquals(Integer.valueOf(VALUE_TO_ADD), valueToRemove);
+        final Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .beforeRemove(testConsumer)
+            .build();
+
+        collection.add(VALUE_TO_ADD);
+        collection.remove(VALUE_TO_ADD);
+    }
+
+    @Test
+    public void testAfterRemoveTrigger() {
+        BiConsumer<Integer, Boolean> testConsumer = (valueToRemove, result) -> {
+            assertEquals(Integer.valueOf(VALUE_TO_ADD), valueToRemove);
+            assertTrue(result);
+        };
+        final Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .afterRemove(testConsumer)
+            .build();
+
+        collection.add(VALUE_TO_ADD);
+        collection.remove(VALUE_TO_ADD);
+    }
+
+    @Test
+    public void testBeforeRemoveAllTrigger() {
+        Consumer<Collection<Integer>> testConsumer = collectionToRemove -> assertEquals(2, collectionToRemove.size());
+        final Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .beforeRemoveAll(testConsumer)
+            .build();
+
+        collection.addAll(LIST_TO_ADD);
+        collection.removeAll(LIST_TO_REMOVE);
+    }
+
+    @Test
+    public void testAfterRemoveAllTrigger() {
+        BiConsumer<Collection<Integer>, Boolean> testConsumer = (collectionToRemove, result) -> {
+            assertEquals(2, collectionToRemove.size());
+            assertTrue(result);
+        };
+        final Collection<Integer> collection = TriggerCollection.from(this.collection)
+            .afterRemoveAll(testConsumer)
+            .build();
+
+        collection.addAll(LIST_TO_ADD);
+        collection.removeAll(LIST_TO_REMOVE);
     }
 }
