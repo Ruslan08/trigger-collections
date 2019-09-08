bb3faf22843c14757d67ea6f564de15736587dc3
Ruslan08 <eto_ya08@mail.ru>
2019-09-08 23:23:54 +0300

    add documentation

--- src/main/java/dev/rus4j/collect/TriggerCollection.java
+++ src/main/java/dev/rus4j/collect/TriggerCollection.java
@@ -57,7 +57,7 @@ public class TriggerCollection implements InvocationHandler {
         return method.invoke(builder.backedCollection, args);
     }
 
-    static class TriggerCollectionBuilder<T> {
+    public static class TriggerCollectionBuilder<T> {
 
         private Collection<T> backedCollection;
 
@@ -73,53 +73,98 @@ public class TriggerCollection implements InvocationHandler {
         private Consumer<Collection<T>> beforeRemoveAll = valueToRemove -> {};
         private BiConsumer<Collection<T>, Boolean> afterRemoveAll = (valueToRemove, result) -> {};
 
-        public TriggerCollectionBuilder(Collection<T> backedCollection) {
+        TriggerCollectionBuilder(Collection<T> backedCollection) {
             if (builder == null) {
                 builder = this;
             }
             this.backedCollection = backedCollection;
         }
 
+        /**
+         * Adds action that will be executed before {@link Collection#add(Object)} method is called.
+         * @param beforeAdd action with element to be added as a parameter
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> beforeAdd(Consumer<T> beforeAdd) {
             this.beforeAdd = beforeAdd;
             return this;
         }
 
+        /**
+         * Adds action that will be executed after {@link Collection#add(Object)} method is called.
+         * @param afterAdd action with element to be added and result as parameters
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> afterAdd(BiConsumer<T, Boolean> afterAdd) {
             this.afterAdd = afterAdd;
             return this;
         }
 
+        /**
+         * Adds action that will be executed before {@link Collection#addAll(Collection)} method is called.
+         * @param beforeAddAll action with collection of elements to be added as a parameter
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> beforeAddAll(Consumer<Collection<T>> beforeAddAll) {
             this.beforeAddAll = beforeAddAll;
             return this;
         }
 
+        /**
+         * Adds action that will be executed after {@link Collection#addAll(Collection)} method is called.
+         * @param afterAddAll action with collection of elements to be added and result as parameters
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> afterAddAll(BiConsumer<Collection<T>, Boolean> afterAddAll) {
             this.afterAddAll = afterAddAll;
             return this;
         }
 
+        /**
+         * Adds action that will be executed before {@link Collection#remove(Object)} method is called.
+         * @param beforeRemove action with element to be removed as a parameter
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> beforeRemove(Consumer<T> beforeRemove) {
             this.beforeRemove = beforeRemove;
             return this;
         }
 
+        /**
+         * Adds action that will be executed after {@link Collection#remove(Object)} method is called.
+         * @param afterRemove action with element to be removed and result as parameters
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> afterRemove(BiConsumer<T, Boolean> afterRemove) {
             this.afterRemove = afterRemove;
             return this;
         }
 
+
+        /**
+         * Adds action that will be executed before {@link Collection#removeAll(Collection)} method is called.
+         * @param beforeRemoveAll action with collection of elements to be removed as a parameter
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> beforeRemoveAll(Consumer<Collection<T>> beforeRemoveAll) {
             this.beforeRemoveAll = beforeRemoveAll;
             return this;
         }
 
+        /**
+         * Adds action that will be executed after {@link Collection#removeAll(Collection)} method is called.
+         * @param afterRemoveAll action with collection of elements to be removed and result as parameters
+         * @return builder
+         */
         public TriggerCollectionBuilder<T> afterRemoveAll(BiConsumer<Collection<T>, Boolean> afterRemoveAll) {
             this.afterRemoveAll = afterRemoveAll;
             return this;
         }
 
+        /**
+         * Creates a proxy instance.
+         * @return proxy instance for the {@link Collection}
+         */
         @SuppressWarnings("unchecked")
         public Collection<T> build() {
             return (Collection<T>) Proxy.newProxyInstance(
